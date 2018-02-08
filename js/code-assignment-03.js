fe43efb33fd7fe233c90e820e7f724b2ce42a935
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-02-08 11:53:11 -0600

    Java Script

---
+++ js/code-assignment-03.js
@@ -0,0 +1,9 @@
+let projectId = 1
+let projecttitle = "Earthquake Map"
+let thumbnail = true
+let projecttitles = ["Earthquake Map", "Map of Earthquake magnitude 4.5 or greater"]
+
+console.log(projectId)
+console.log(projecttitle)
+console.log(thumbnail)
+console.log(projecttitles)
