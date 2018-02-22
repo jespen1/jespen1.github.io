bc20b7f2768d6d637c24a432f1c89ca799ae2617
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-02-22 11:51:00 -0600

    code 5

---
+++ js/code-assignment-05.js
@@ -0,0 +1,21 @@
+let projects = [ {
+  id: 1,
+  url: 'http://arcg.is/2EOzxhz'
+  title: 'Book Assignment 1'
+  desc: 'Tour of Lakeview Louisiana, where I went to high school.',
+  thumb: true,
+  keywords: ['Lakeview', 'Louisiana', 'story map', 'arcgis online']
+},
+{
+  id:2,
+  url:'https://www.arcgis.com/apps/View/index.html?appid=dc4d335ae4d64fa7a596931fc7f673b2',
+  title:'Book Assignment 2',
+  desc:'4.5+ Magnitude Earthquakes around the world.',
+  thumb: false,
+  keywords: ['USGS', 'earthquakes', 'arcgis online']
+}  ]
+
+for (let i=0; i< projects.length; i++) {
+  let x = i + 1
+  console.log('ss-project-' + x = '.png');
+}
