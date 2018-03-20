ab0f83dcff97f2ca474ce29fb705aed835b16812
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-03-20 11:41:19 -0500

    update

--- js/code-assignment-06a.js
+++ js/code-assignment-06a.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
 createThumbnail1(projectData[i].thumb, projectsData[i].id)
-
+}
 
 
 function createTitle (title) {
@@ -12,4 +12,5 @@ function createThumbnail1 (thumb, id) {
     console.log('BookAssignment-' + id + '.png')
   } else {
     console.log('logo.png')
+  }
 }
--- js/code-assignment-07a.js
+++ js/code-assignment-07a.js
@@ -1,3 +1,4 @@
 for (let i = 0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
 createThumbnail1(projectData[i].thumb, projectsData[i].id)
+}
