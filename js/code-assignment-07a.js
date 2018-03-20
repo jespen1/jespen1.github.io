72c11d9fb1e0a9c1eab4e63e62fe1be20e1bb86d
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-03-20 11:32:46 -0500

    update 6a and 7a

--- js/code-assignment-07a.js
+++ js/code-assignment-07a.js
@@ -1,35 +1,3 @@
-let projectData = []
-
-projectData[0] = {
-  id: 1,
-  url: 'http://arcg.is/2EOzxhz',
-  title: 'Book Assignment 1',
-  desc: 'Tour of Lakeview Louisiana, where I went to high school.',
-  thumb: true,
-  keywords: ['Lakeview', 'Louisiana', 'story map', 'tour']
-}
-
-projectData[1] = {
-  id:2,
-  url:'https://www.arcgis.com/apps/View/index.html?appid=dc4d335ae4d64fa7a596931fc7f673b2',
-  title:'Book Assignment 2',
-  desc:'4.5+ Magnitude Earthquakes around the world.',
-  thumb: false,
-  keywords: ['Earthquakes', 'USGS', 'ArcGIS']
-}
-
 for (let i = 0; i < projectData.length; i++) {
-  if (projectData[i].thumb === true) {
-  console.log('BookAssignment-' + projectData[i].id + '.png')
-} else {
-  console.log('logo.png')
-  }
-}
-
-for (let i = 0; i < projectData.length; i++) {
-  if (projectData[i].title === '') {
-    console.log('No title')
-  } else {
-    console.log(projectData[i].title)
-  }
-}
+  createTitle(projectData[i].title)
+createThumbnail1(projectData[i].thumb, projectsData[i].id
---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function createTitle (title) {
+  console.log('BookAssignment-' + title)
+}
+function createThumbnail1 (thumb, id) {
+  if (thumb === true) {
+    console.log('BookAssignment-' + id + '.png')
+  } else {
+    console.log('logo.png')
+}
