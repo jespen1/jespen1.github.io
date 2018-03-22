74250bebb9a3bfcf238528f2407bca92e24e3c1e
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-03-22 10:31:00 -0500

    update

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,3 @@
-let projectData = []
-
-projectData[0] = {
-  id: 1,
-  url: 'http://arcg.is/2EOzxhz',
-  title: 'Book Assignment 1',
-  desc: 'Tour of Lakeview Louisiana, where I went to high school.',
-  thumb: true,
-  keywords: ['Lakeview', 'Louisiana', 'story map', 'tour']
-}
-
-projectData[1] = {
-  id:2,
-  url:'https://www.arcgis.com/apps/View/index.html?appid=dc4d335ae4d64fa7a596931fc7f673b2',
-  title:'Book Assignment 2',
-  desc:'4.5+ Magnitude Earthquakes around the world.',
-  thumb: false,
-  keywords: ['Earthquakes', 'USGS', 'ArcGIS']
-}
-
 function createTitle (title) {
   console.log('BookAssignment-' + title)
 }
