ef81f66a5f32edf555ce6671f4c30715fb013abc
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-03-22 11:54:48 -0500

    update all

---
+++ map/webmap-assignment-01.js
@@ -0,0 +1,19 @@
+//let JEmap = L.map('leafletwebmap1').setView([29.854929,-89.990625], 12)
+let JEmap = L.map('leafletwebmap1').setView([29.854929,-89.990625], 12)
+
+let basemap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+L.tileLayer(basemap).addTo(JEmap)
+let marker = L.marker([29.854929,-89.990625]).addTo(JEmap)
+let polygon = L.polygon([
+  [29.851677, -89.982986],
+  [29.887403, -89.975090],
+  [29.813303, -90.026760]
+]).addTo(JEmap);
+
+polygon.bindPopup('Belle Chasse')
+marker.bindPopup('Belle Chasse High School')
+
+function logCurrentCoordinates (event) {
+  console.log('You clicked the map at ' + event.latlng)
+}
+mymap.on('click', logCurrentCoordinates)
