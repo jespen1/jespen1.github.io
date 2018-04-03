d076ce06906704aba76b9aa39c28a9c74a7220ad
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-04-03 10:57:54 -0500

    update

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -2,10 +2,22 @@
 let JEmap = L.map('leafletwebmap1').setView([29.854929,-89.990625], 12)
 
 let basemap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-
 L.tileLayer(basemap).addTo(JEmap)
 
-let marker = L.marker([29.854929,-89.990625]).addTo(JEmap)
+let JEpoint = L.marker([29.854929,-89.990625]).addTo(JEmap)
+
+var pointA = new L.LatLng(29.854929,-89.990625);
+var pointB = new L.LatLng(29.82469,-90.016914);
+var pointList = [pointA, pointB];
+
+var firstpolyline = new L.Polyline(pointList, {
+    color: 'red',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+});
+firstpolyline.addTo(JEmap);
+
 
 let polygon = L.polygon([
   [29.851677, -89.982986],
@@ -20,3 +32,7 @@ function logCurrentCoordinates (event) {
   console.log('You clicked the map at ' + event.latlng)
 }
 mymap.on('click', logCurrentCoordinates)
+
+mymap.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+})
