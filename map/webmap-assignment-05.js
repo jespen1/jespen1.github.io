8385aa7b35d8c10b4014ff5ee3297a3b316f38ba
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-04-17 11:42:51 -0500

    updates on 5

--- map/webmap-assignment-05.js
+++ map/webmap-assignment-05.js
@@ -1,8 +1,8 @@
-let JEmap5 = L.map('leafletwebmap5').setView([29.9511, -90.0715], 8)
+let JEMap5 = L.map('leafletwebmap5').setView([29.9511, -90.0715], 8)
 
-let JEColorMap1 = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}').addTo(JEMap5)
+let JEColorMap1 = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(JEMap5)
 let JELightMap1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(JEMap5)
-let JEDarkMap1 = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}').addTo(JEMap5)
+let JEDarkMap1 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(JEMap5)
 
 let myBasemaps = {
   'Color Basemap': JEColorMap1,
@@ -13,8 +13,8 @@ let myBasemaps = {
 
 JEGeojsonStyle = function (state) {
   let Auth = state.properties.Auth
-  let stateColor = 'red'
-  if (auth== 'Franchise' ) {stateColor = 'Blue'}
+  let stateColor = 'blue'
+  if (Auth== 'Franchise' ) {stateColor = 'pink'}
   let myStyle = {
     color: stateColor,
     weight: 1,
@@ -28,4 +28,4 @@ JEGeojsonOptions = {
 }
 
 L.control.layers(myBasemaps).addTo(JEMap5)
-L.geoJSON(foodtruck, myGeojsonOptions).addTo(JEmap5)
+L.geoJSON(foodtruck, JEGeojsonOptions).addTo(JEMap5)
