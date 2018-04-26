72381db4f209ea281cf7706ef7e8e5e7a248b66e
jespen1 <35343426+jespen1@users.noreply.github.com>
2018-04-26 11:39:15 -0500

    update

--- NOLAschools/webmap-assignment-nolaschoolsFP.js
+++ NOLAschools/webmap-assignment-nolaschoolsFP.js
@@ -1,4 +1,4 @@
-let JEMapFP = L.map('leafletwebmapFP').setView([29.9511, -90.0715], 8)
+let JEMapFP = L.map('leafletwebmapFP').setView([29.9511, -90.0715], 4)
 
 let JEColorMap1 = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(JEMapFP)
 let JELightMap1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(JEMapFP)
@@ -15,8 +15,8 @@ L.control.layers(myBasemaps).addTo(JEMapFP)
 
 function createCustomIcon (feature, latlng) {
   let JEMarkerIcon = L.icon({
-  iconUrl: 'point.png',
-  iconSize: [38, 95], // size of the icon
+  iconUrl: 'pencil.png',
+  iconSize: [50, 50], // size of the icon
   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
   })
@@ -35,5 +35,5 @@ JEGeojsonOptions = {
   pointToLayer: createCustomIcon
 }
 
-L.control.layers(myBasemaps).addTo(JEMapFP)
+
 L.geoJSON(nolaschools, JEGeojsonOptions).addTo(JEMapFP)
